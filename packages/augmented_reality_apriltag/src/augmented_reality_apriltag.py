fa38015b3b13c5566b627686b9637e0aa558279c
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2020-11-09 12:24:09 +0100

    Added suitable in-code documentation

--- packages/augmented_reality_apriltag/src/augmented_reality_apriltag.py
+++ packages/augmented_reality_apriltag/src/augmented_reality_apriltag.py
@@ -55,9 +55,18 @@ class ARNode(DTROS):
         self.image_sub = rospy.Subscriber('/' + self.veh+ '/camera_node/image/compressed', CompressedImage, self.callback)
         # Publish the rendered image to a new topic
         self.augmented_pub = rospy.Publisher('~image/compressed' , CompressedImage, queue_size=1)
+
         self.log(node_name + ' initialized and running')
 
     def callback(self, ros_image):
+        '''
+            MAIN TASK
+            Converse the image from the camera_node to a cv2 image. Then, detects 
+            the apriltags position and retrieves the homography. With the homography
+            and the intrinsic camera matrix, it computes the projection matrix. Finally,
+            the provided Renderer class returns an image with the model rendered on it,
+            which is then published to a new topic
+        '''
         # Convert to cv2 image
         image = self.readImage(ros_image)
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -67,15 +76,15 @@ class ARNode(DTROS):
         cam_params = (K[0,0], K[1,1], K[0,2], K[1,2])
 
         # Detect apriltags
-        detections = self.at_detector.detect(gray_image, True, cam_params, tag_size=0.065)
+        detections = self.at_detector.detect(gray_image, estimate_tag_pose=True, camera_params=cam_params, tag_size=0.065)
 
-        # Render model in image
+        # Render a model in each of the detected Apriltags
         rendered_image = image
         for tag in detections:
             # Extract homography
-            H = np.array(tag.homography).reshape((3,3))           
+            H = np.array(tag.homography).reshape((3,3))         
 
-            # Obtain homography
+            # Obtain Projection matrix
             projection_matrix = self.projection_matrix(K, H)
 
             # Render the model
@@ -89,6 +98,10 @@ class ARNode(DTROS):
 
 
     def setCamInfo(self, calib_data):
+        '''
+            Introduces the camera information from the dictionary
+            obtained reading the yaml file to a CameraInfo object
+        ''' 
         cam_info = CameraInfo()
         cam_info.width = calib_data['image_width']
         cam_info.height = calib_data['image_height']
@@ -103,25 +116,34 @@ class ARNode(DTROS):
     
     def projection_matrix(self, intrinsic, homography):
         """
-            Write here the compuatation for the projection matrix, namely the matrix
-            that maps the camera reference frame to the AprilTag reference frame.
+            Method that computes a Projection matrix from 3D world to image plane.
+            Args:
+                intrinsic: (np.array) Camera matrix
+                homography: (np.array) 2D Homography obtained by detecting the apriltag position
+            Returns:
+                P: Projeciton matrix
+
         """
         # Extract parameters
         K = intrinsic
         K_inv = np.linalg.inv(K)
         H = homography
+
         # Compute 2D Rt matrix
         Rt = K_inv @ H
+        # Normalize so that ||R1|| = 1
         Rt = Rt / np.linalg.norm(Rt[:,0])
+        t = np.array(Rt[:,2]).reshape((3,1))
         # Extract rotation vectors
         R1 = np.array(Rt[:,0]).reshape((3,1))
         R2 = np.array(Rt[:,1]).reshape((3,1))
         # Compute third rotation vector to be orthogonal to the other two
         R3 = np.array(np.cross(Rt[:,0],Rt[:,1])).reshape((3,1))
+        # Construct 3D rotation matrix
         R = np.concatenate((R1,R2,R3),axis=1)
+        # Perform polar decomposition to enforce orthogonality
         U, S, Vt = np.linalg.svd(R)
         R = U @ Vt
-        t = np.array(Rt[:,2]).reshape((3,1))
         # Compute projection matrix
         P = K @ (np.concatenate((R,t),axis=1))
 
